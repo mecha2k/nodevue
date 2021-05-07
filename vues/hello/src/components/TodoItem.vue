f36a1524c3420d427858516516c876e7373d1a4f
mecha2k <mecha2k@naver.com>
2021-05-07 10:34:02 +0900

    vue hello

---
+++ vues/hello/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>{{ msg }}</h3>
+    <li>TodoItem list...</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    msg: String
+  }
+}
+</script>
--- vues/hello/src/views/Home.vue
+++ vues/hello/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <h3>
+      <span v-once>{{ nameTag }}</span>
+      <span v-if="isVisible">:: {{ visit }}</span>
+    </h3>
+    <h3>{{ propVisit }}</h3>
+    <input type="text" @keyup="visit++" v-model="nameTag" />
+    <button @click="visitCount()">visit count</button>
+    <span v-html="nameTag"></span>
+    <hr />
+    <button v-bind:disabled="isDisabled">Disabled</button>
+    <TodoItem msg="Parent 'Home' message props." />
+    <hr />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,11 +20,33 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue"
+import TodoItem from "@/components/TodoItem.vue"
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoItem
+  },
+  data() {
+    return {
+      nameTag: "<strong style='color: red;'>홍길동</strong>",
+      visit: 0,
+      isVisible: false,
+      isDisabled: false
+    }
+  },
+  methods: {
+    visitCount() {
+      this.visit++
+      this.isVisible = !this.isVisible
+      this.isDisabled = !this.isDisabled
+    }
+  },
+  computed: {
+    propVisit() {
+      return `Hi~ ${this.name}, you are ${this.visit} times visited.`
+    }
   }
 }
 </script>
