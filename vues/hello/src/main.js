1593d34412bf2f1e5141d369713d3a103091d0b0
mecha2k <mecha2k@naver.com>
2021-05-08 17:47:26 +0900

    component register

--- vues/hello/src/main.js
+++ vues/hello/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from "vue"
 import axios from "axios"
+import lodash from "lodash"
 
 import App from "./App.vue"
 import router from "./router"
+import componentRegister from "./components/_global"
 
 const app = createApp(App)
 
 app.config.globalProperties.axios = axios
-// app.prototype._
+app.config.globalProperties.lodash = lodash
+
+componentRegister(app)
 
 app.use(router).mount("#app")
